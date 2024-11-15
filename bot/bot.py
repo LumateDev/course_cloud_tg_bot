28b0867ebb88e450ab14127c840dafc20487dbc7
LumateDev <sacnhous557@mail.ru>
2024-11-15 14:27:26 +0300

    add start command, echo handler

--- bot/bot.py
+++ bot/bot.py
@@ -4,9 +4,11 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from dotenv import load_dotenv
 import os
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, html, types
 import requests
 from datetime import datetime
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 import logging
 
 load_dotenv()
@@ -14,6 +16,28 @@ BACKEND_URL = os.getenv('BACKEND_URL')
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    """
+    Handler will forward receive a message back to the sender
+
+    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    """
+    try:
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
 
 async def main() -> None:
     bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
