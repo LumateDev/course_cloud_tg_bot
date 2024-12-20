6f17804c2ce6db9b3e7a43e98abdd3e5cf942647
Lumate <sanchous557@mail.ru>
2024-12-20 03:52:07 +0300

    update models

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, UniqueConstraint
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, UniqueConstraint, BigInteger
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from .database import Base  # Базовый класс для моделей
@@ -20,7 +20,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
-    telegram_id = Column(Integer, unique=True, index=True, nullable=False)
+    telegram_id = Column(BigInteger, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=True)
     created_at = Column(DateTime, server_default=func.now())
 
